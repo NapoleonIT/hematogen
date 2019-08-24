b58c2577bf97c6b303fef1cbdeb98177baef5598
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2019-08-24 14:40:09 +0500

    add to prev commit

---
+++ codegen/config/config.py
@@ -0,0 +1,15 @@
+import os
+from typing import get_type_hints, Type, List
+
+from enum import Enum
+
+
+class BaseCodegenConfig(object):
+
+    def __init__(self, filename: str, *args, **kwargs):
+        with open
+
+
+
+class PythonCodegenConfig(BaseCodegenConfig):
+    pass
